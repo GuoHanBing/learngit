b5d51643b4fb6f0c6adf4c2ebf131c6953c08584
GuoHanBing <995316689@qq.com>
2020-08-02 14:32:14 +0800

    git tracks changes

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static void main(String [] args) {
-		System.out.println("测试分支Git has a mutable index called stage.");
+		System.out.println("测试分支Git 
+			has a mutable index called stage.
+			Git tracks changes.");
 	}
 }
