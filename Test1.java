89e92a3d704336817a38e2f30ace34408cfcc554
GuoHanBing <995316689@qq.com>
2020-08-02 14:50:20 +0800

    删除了Test1.java

--- Test1.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld{
-	public static void main(String [] args) {
-		System.out.println("Git is a distributed version control system.Git is free software distributed under the GPL.");
-	}
-}
