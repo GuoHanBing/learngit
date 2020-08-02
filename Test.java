065e6e6890268f4a6b61c6ea31a998144d1698b5
GuoHanBing <995316689@qq.com>
2020-08-02 14:40:30 +0800

    撤销修改

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 	public static void main(String [] args) {
 		System.out.println("测试分支Git 
 			has a mutable index called stage.
-			Git tracks changes.");
+			Git tracks changes.
+			添加了
 	}
 }
