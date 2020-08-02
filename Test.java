122f872494f6e7877748792ceb9915e495d9a537
GuoHanBing <995316689@qq.com>
2020-08-02 15:30:54 +0800

    推送到版本库

--- Test.java
+++ Test.java
@@ -1,8 +1,6 @@
 public class HelloWorld{
 	public static void main(String [] args) {
-		System.out.println("测试分支Git 
-			has a mutable index called stage.
-			Git tracks changes.
-			添加了
+		System.out.println("推送到版本库");
+
 	}
 }
